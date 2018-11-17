697b73424bc3175389d0ce8ee60fa704d096c6f7
Andrew Erickson <aerickson@gmail.com>
2018-11-17 00:40:47 -0800

    build script: do cleanup, add comments

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ HACKNAME="jarextractor"
 PKGNAME="${HACKNAME}"
 PKGVER="0.1.1"
 
+# cleanup
+rm *.bin
+
+# build update bin
 kindletool create ota2 -d kindle5 -C install.sh update_${PKGNAME}_${PKGVER}_install.bin
