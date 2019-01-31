7b3aa2c276daa4d5ee31351a5fcd1f4b617ff4bc
Andrew Erickson <aerickson@gmail.com>
2019-01-30 19:00:56 -0800

    build.sh: don't die if no files to cleanup

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ PKGNAME="${HACKNAME}"
 PKGVER="0.1.2"
 
 # cleanup
-rm *.bin
+rm *.bin || true
 
 # build update bin
 kindletool create ota2 -d kindle5 -C install.sh update_${PKGNAME}_${PKGVER}_install.bin
